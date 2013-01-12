98b6de04a20c1559d8f2c9ec629606e469b9baf5
Mo Nianliang <nianliang@gmail.com>
2013-01-12 14:33:23 +0800

    Fix ARC compile error

--- UIImage+PDF/PDFView.h
+++ UIImage+PDF/PDFView.h
@@ -7,6 +7,35 @@
 
 #import <UIKit/UIKit.h>
 
+////////////////////////////////////////////////////////////////////////////////
+//  ARC Helper
+//
+//  Version 2.1
+//
+//  Created by Nick Lockwood on 05/01/2012.
+//  Copyright 2012 Charcoal Design
+//
+//  Distributed under the permissive zlib license
+//  Get the latest version from here:
+//
+//  https://gist.github.com/1563325
+//
+
+#ifndef ah_retain
+#if __has_feature(objc_arc)
+#define ah_retain self
+#define ah_dealloc self
+#define release self
+#define autorelease self
+#else
+#define ah_retain retain
+#define ah_dealloc dealloc
+#define __bridge
+#endif
+#endif
+
+//  ARC Helper ends
+
 
 @interface PDFView : UIView 
 {
